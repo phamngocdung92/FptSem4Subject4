e29cd5ca6d5cd6b08e457175afa3c643f66d550d
phamngocdung <phamngoc.dung@vnpt.vn>
2024-08-13 09:09:50 +0700

    init

---
+++ Subject4/src/main/java/fptSem4/Subject4/PhamNgocDungApplication.java
@@ -0,0 +1,13 @@
+package fptSem4.Subject4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamNgocDungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamNgocDungApplication.class, args);
+	}
+
+}
---
+++ Subject4/src/test/java/fptSem4/Subject4/PhamNgocDungApplicationTests.java
@@ -0,0 +1,13 @@
+package fptSem4.Subject4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamNgocDungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
